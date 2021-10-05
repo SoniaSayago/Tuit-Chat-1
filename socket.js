d8a89f8415336a9ec58ba675ff6399c6993d870d
Andy164 <apenarandao@ufpso.edu.co>
2021-10-05 18:00:56 -0500

    Change to env

--- socket.js
+++ socket.js
@@ -2,8 +2,8 @@ import { io } from 'socket.io-client';
 
 const URL = process.env.NEXT_PUBLIC_VERCEL_URL;
 console.log('--------------------------------------------');
-console.log('http://localhost:3000');
-const socket = io('http://localhost:3000', {
+console.log(URL);
+const socket = io(URL, {
   path: '/api/socketio',
 });
 
