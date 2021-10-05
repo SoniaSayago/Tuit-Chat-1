5677b2a230471ffb7b3876f7d7a4bad38faaf44b
Andy164 <apenarandao@ufpso.edu.co>
2021-10-05 17:59:38 -0500

    Delete autocnnect

--- socket.js
+++ socket.js
@@ -2,10 +2,9 @@ import { io } from 'socket.io-client';
 
 const URL = process.env.NEXT_PUBLIC_VERCEL_URL;
 console.log('--------------------------------------------');
-console.log(process.env.NEXT_PUBLIC_VERCEL_URL);
-const socket = io(URL, {
+console.log('http://localhost:3000');
+const socket = io('http://localhost:3000', {
   path: '/api/socketio',
-  autoConnect: false,
 });
 
 socket.onAny((event, ...args) => {
