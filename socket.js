bcfceb3ba4543e9c88ce4ab596a5d7244da3e8d7
Andy164 <apenarandao@ufpso.edu.co>
2021-10-11 11:53:26 -0500

    Try with api/socket.io

--- socket.js
+++ socket.js
@@ -4,6 +4,7 @@ const URL = process.env.NEXT_PUBLIC_VERCEL_URL;
 console.log('--------------------------------------------');
 console.log(URL);
 const socket = io('https://tuit-tuit.herokuapp.com', {
+  path: '/api/socketio',
   autoConnect: false,
 });
 
